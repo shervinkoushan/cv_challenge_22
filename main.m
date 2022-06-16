4465006d90b2f394b76012aae4292f0ef1d0c07b
Shervin Koushan <shervink@stud.ntnu.no>
2022-06-16 14:58:35 +0200

    select image callback

--- main.m
+++ main.m
@@ -174,64 +174,18 @@ function TabSellectCallback(~,~,SelectedTab)
 end
 
 %%   Select Image File Callback
-    function SelectImageCallback(~,~)
+function SelectImageCallback(~, ~)
+     folder_path='lib/';
+     file_names = ['simple-room.png','uhren-turm.jpg',...
+                   'metro-station.png','oil-painting.png',...
+                   'sagrada_familia.png','shopping-mall.png'];
+     hlist = {handles.axes1, handles.axes2, handles.axes3, handles.axes4};
     
-    %   Get TabHandles from guidata and set some varables
-        TabHandles = guidata(gcf);
-        NumberOfTabs = size(TabHandles,1)-2;
-        PanelWidth = TabHandles{NumberOfTabs+1,2};
-        PanelHeight = TabHandles{NumberOfTabs+1,3};
-
-    %   Two persistent varables are needed
-        persistent StartPicDirectory hImageAxes
-        
-    %   Initilize the StartPicDirectory if first time through
-        if isempty(StartPicDirectory)
-            StartPicDirectory = cd;
-        end
-    
-    %   Get the file name from the user
-        [PicNameWithTag, PicDirectory] = uigetfile({'*.jpg;*.tif;*.png','Image Files'},...
-            'Select an image file',StartPicDirectory);
-
-        if PicNameWithTag == 0,
-            %   If User cancels then display error message
-                errordlg('You should select an Image File');
-            return
-        end
-        
-    %   Set the default directory to the currently selected directory
-        StartPicDirectory = PicDirectory;
-
-    %   Build path to file
-        PicFilePath = strcat(PicDirectory,PicNameWithTag);
-            
-    %   Load the image
-        InitPicRGB = imread(PicFilePath);
-
-    % Get handle of default panel content
-        h1 = TabHandles{size(TabHandles,1),1};
-        
-    %   Delete the previous panel content
-        if ishandle(h1)
-            delete(h1);             % Delete the default content
-        else
-            delete(hImageAxes);     % Delete the previous image
-        end
-        
-    %   Set the axes and display the image    
-        ImgOffset = 40;
-        hImageAxes = axes('Parent', TabHandles{2,1}, ...
-            'Units', 'pixels', ...
-            'Position', [ImgOffset ImgOffset ...
-                PanelWidth-2*ImgOffset PanelHeight-2*ImgOffset]);
-            
-        image(InitPicRGB,'Parent', hImageAxes);
-
-    %   Make Image Tab active
-        TabSellectCallback(0,0,2);
-    
-    end
+     for i = 1 : 2
+        axes(hlist{i});
+        imshow(imread(folder_path+file_names(i)), []);     
+     end
+end
 
 %%   Open Image File Callback
     function OpenImageCallback(~,~)
