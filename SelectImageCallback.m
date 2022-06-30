7598cda44caf7c77999bd5e7bd9e54a36e60ca48
Shervin Koushan <shervink@stud.ntnu.no>
2022-06-30 12:23:59 +0200

    refactor, extract functions to files

---
+++ SelectImageCallback.m
@@ -0,0 +1,66 @@
+%%   Select Image Callback
+function SelectImageCallback(~, ~)
+
+    dialog_pos_x=500;
+    dialog_pos_y=200;
+    dialog_width=500;
+    dialog_height=450;
+    
+    d = dialog('Position',[dialog_pos_x dialog_pos_y dialog_width dialog_height],'Name','Select image');
+       
+    txt = uicontrol('Parent',d,...
+           'Style','text',...
+           'Position',[dialog_width/2-150 dialog_height-90 300 40],...
+           'String','Click on the image you would like to use',...
+            'FontName', 'arial',...
+            'FontWeight', 'bold',...
+            'FontSize', 10);
+ 
+    
+    btn = uicontrol('Parent',d,...
+           'Position',[dialog_width/2-35 20 70 35],...
+           'String','Close',...
+           'Callback','delete(gcf)',...
+           'Style', 'pushbutton',...
+            'HorizontalAlignment', 'center',...
+            'FontName', 'arial',...
+            'FontWeight', 'bold',...
+            'FontSize', 9);
+       
+    
+     button_width=100;
+     button_height=100;
+     folder_path='lib/';
+     file_names = ["simple-room.png" "oil-painting.png" "shopping-mall.png" ...
+                   "uhren-turm.jpg" "metro-station.png" "sagrada_familia.png" ];
+     pos_x=[50 200 350 50 200 350];
+     pos_y=[250 250 250 100 100 100];
+     
+     for i = 1 : length(file_names)
+        filepath=append(folder_path,file_names(i));
+        image=imread(filepath);
+        [r,c,~]=size(image); 
+        x=ceil(r/90); 
+        y=ceil(c/90); 
+        g=image(1:x:end,1:y:end,:);
+        g(g==255)=5.5*255;
+        image_button = uicontrol('Parent',d,...
+                        'Position',[pos_x(i) pos_y(i) button_width button_height],...
+                        'CData',g,...
+                        'Callback',{@image_callback, filepath});
+     end
+
+    PicFilePath="";
+    % Wait for d to close before running to completion
+    uiwait(d);
+    
+      function image_callback(~,~,filepath)
+          PicFilePath=filepath;
+          delete(gcf);
+      end
+  
+    if PicFilePath~=""
+        ShowImageInTab2(PicFilePath);
+    end
+
+end
---
+++ ShowImageInTab2.m
@@ -0,0 +1,37 @@
+function ShowImageInTab2(file_path)
+    
+    %   Get TabHandles from guidata and set some varables
+        TabHandles = guidata(gcf);
+        NumberOfTabs = size(TabHandles,1)-2;
+        PanelWidth = TabHandles{NumberOfTabs+1,2};
+        PanelHeight = TabHandles{NumberOfTabs+1,3};
+
+        persistent hImageAxes
+            
+    %   Load the image
+        InitPicRGB = imread(file_path);
+
+    % Get handle of default panel content
+        h1 = TabHandles{size(TabHandles,1),1};
+        
+    %   Delete the previous panel content
+        if ishandle(h1)
+            delete(h1);             % Delete the default content
+        else
+            delete(hImageAxes);     % Delete the previous image
+        end
+        
+    %   Set the axes and display the image    
+        ImgOffset = 40;
+        hImageAxes = axes('Parent', TabHandles{2,1}, ...
+            'Units', 'pixels', ...
+            'Position', [ImgOffset ImgOffset ...
+                PanelWidth-2*ImgOffset PanelHeight-2*ImgOffset]);
+            
+        image(InitPicRGB,'Parent', hImageAxes);
+
+    %   Make Image Tab active
+        TabSelectCallback(0,0,2);
+    
+end
+       
