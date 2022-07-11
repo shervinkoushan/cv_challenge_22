b59a4805b4cd72eba44608f351888d6ac055fdd9
bala_meszaros <bala.meszaros@gmail.com>
2022-07-11 15:44:24 +0200

    Not working part.

---
+++ untitled.m
@@ -0,0 +1,44 @@
+%I = imread("Show the five planes.png");
+I = imread("lib\oil-painting.png");
+%figure;
+%imshow(I)
+
+v = [775,500];
+ir = [340,960,960,340; 182,182,661,661];
+im_size = [829, 1152];
+
+
+
+or = outer_rectangle(v, ir, im_size);
+% 
+[back_rec, top_rec, bottom_rec, left_rec, right_rec] = create5rect(im_size, v, ir, or);
+
+[bigim, back_b, top_b, bot_b, left_b, right_b] = createBigim(back_rec, top_rec, bottom_rec, left_rec, right_rec, I);
+
+
+right_b = right_b'
+right_b(2,2) = 1;
+
+original_points = [1000, 500; 1151, 200; 1151, 800; 1000, 700]; 
+fixed_points = [0, 0; 2000, 0; 2000, 2000; 0, 2000]
+trans = fitgeotrans(original_points, fixed_points, 'projective');
+img_d = im2double(I);
+
+
+% Set trensformation settings
+RA = imref2d(size(img_d));
+
+[img_trans, BA] = imwarp(img_d, RA, trans);
+
+figure
+imshow(img_trans)
+
+%%crop_settings = [round(abs(BA.XWorldLimits(1)+1)), round(abs(BA.YWorldLimits(1)+1)), 680-260, 680-260];
+
+% fin_image = imcrop(img_trans, crop_settings);
+% 
+% figure
+% imshow(fin_image)
+% hold on
+% points = transformPointsInverse(trans, original_points);
+%scatter(points())
