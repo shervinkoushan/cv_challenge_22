4b6dfbae9f116b9a9b6958000548e6ed9f74f61d
Nilstan <nils.stanelle@gmail.com>
2022-07-12 10:29:51 +0200

    Small clean up

--- segmentation.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = segmentation(inputArg1,inputArg2)
-%SEGMENTATION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
