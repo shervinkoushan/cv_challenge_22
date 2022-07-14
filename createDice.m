d5a795d903ec404dc7d0606d12ce094e2225ba9d
Nilstan <nils.stanelle@gmail.com>
2022-07-14 11:04:47 +0200

    Created comments in createDice

--- createDice.m
+++ createDice.m
@@ -1,32 +1,41 @@
 function [new_img] = createDice(back, top, bot, left, right)
+%This function builds the 3D dice,  by taking the five different side
+%planes and warping the wanted plane pivture on it. As a bonus also a flat
+%representation of the dice is created. 
+%% Flat representation of 3D dice
+
 [Y_1 ,X_1, Z] = size(top);
 [Y_2 ,X_2, ~] = size(left);
 [Y_3 ,X_3, ~] = size(right);
 [Y_4 ,X_4, ~] = size(bot);
 [Y_5 ,X_5, ~] = size(back);
 
+%Generating the image
 new_img = zeros(Y_1 + Y_5 + Y_4, X_2 + X_3 + X_5, Z);
 
+%Placing the given image at the correct spots
 new_img(1:Y_1, X_2:(X_1+X_2)-1, :) = im2double(top);
-
 new_img(Y_1:(Y_1+Y_2)-1, 1:X_2, :) = im2double(left);
-
 new_img(Y_1:(Y_1+Y_3)-1, (X_1+X_2):(X_1+X_2+X_3)-1, :) = im2double(right);
-
 new_img((Y_1+Y_2):(Y_1+Y_2+Y_4)-1, X_2:(X_2+X_4)-1, :) = im2double(bot);
-
 new_img(Y_1:(Y_1+Y_5)-1, X_2:(X_2+X_5)-1, :) = im2double(back);
 
 
+%% Creating the dice
 
+%The size of the back image is used to generate the plane the picture is
+%placed on.
 [y_b, x_b, ~] = size(back);
 [back_x,back_y] = meshgrid(1:x_b,1:y_b);
+%Multiplication by 2 is to push the image forward so that we aviod an edge
+%in the dice.
 back_z = 2.*ones(y_b,x_b);
 
 [y, x, ~] = size(top);
 [top_x,top_z] = meshgrid(1:x,1:y);
 top_y = 2.*ones(y,x);
 top = imrotate(top, 180);
+top = flipdim(top,2);
 
 [y, x, ~] = size(bot);
 [bot_x,bot_z] = meshgrid(1:x,1:y);
@@ -37,31 +46,20 @@ bot_y = (y_b-1).* ones(y,x);
 left_x = 2.*ones(x,y);
 left = imrotate(left, 90);
 
-
 [y, x, ~] = size(right);
 [right_y,right_z] = meshgrid(1:y,1:x);
 right_x = (x_b-1).* ones(x,y);
 right = imrotate(right, 90);
-
-%!!!!This should probably be done in get5planes!!!!
 right = flipdim(right,1);
 
-%back
+%Puts the given plane image onto the given plane
 warp(back_x, back_y, back_z, back);
-
-%top
 warp(top_x, top_y, top_z, top);
-
-%bot
 warp(bot_x, bot_y, bot_z, bot);
-
-%left
 warp(left_x, left_y, left_z, left);
-
-%right
 warp(right_x, right_y, right_z, right);
 
-
+%% Camera movement in the dice
 axis equal;  % make X,Y,Z dimentions be equal
 axis vis3d;  % freeze the scale for better rotations
 axis off;    % turn off the stupid tick marks
