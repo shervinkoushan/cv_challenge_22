6847509e24cbdf08e2832350a2176ac0119daf9d
Shervin Koushan <shervink@stud.ntnu.no>
2022-07-15 23:27:42 +0200

    clean up code

--- show_instructions.m
+++ show_instructions.m
@@ -22,44 +22,16 @@ function show_instructions(~, ~)
     btn_left=40;
     btn_middle=75;
     btn_right=110;
-    axes_w = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_middle 300 btn_len btn_len]);
-    axes_a = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_left 270 btn_len btn_len]);
-    axes_s = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_middle 270 btn_len btn_len]);
-    axes_d = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_right 270 btn_len btn_len]);    
-    axes_q = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_left 200 btn_len btn_len]);    
-    axes_e = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_right 200 btn_len btn_len]);    
-    axes_left = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_left 100 btn_len btn_len]);    
-    axes_up = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_middle 130 btn_len btn_len]);    
-    axes_right = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_right 100 btn_len btn_len]);    
-    axes_down = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [btn_middle 100 btn_len btn_len]);    
-
-    % The buttons
-    [w,alpha_w] = get_key_image('w');
-    [a,alpha_a] = get_key_image('a');
-    [s,alpha_s] = get_key_image('s');
-    [d,alpha_d] = get_key_image('d');
-    [q,alpha_q] = get_key_image('q');
-    [e,alpha_e] = get_key_image('e');
-    [left,alpha_left] = get_key_image('left');
-    [right,alpha_right] = get_key_image('right');
-    [down,alpha_down] = get_key_image('down');
-    [up,alpha_up] = get_key_image('up');
-
-    
-    
-    % Display the buttons
-    image(w,'AlphaData',alpha_w,'Parent',axes_w);
-    image(a,'AlphaData',alpha_a,'Parent',axes_a);
-    image(s,'AlphaData',alpha_s,'Parent',axes_s);
-    image(d,'AlphaData',alpha_d,'Parent',axes_d);
-    image(q,'AlphaData',alpha_q,'Parent',axes_q);
-    image(e,'AlphaData',alpha_e,'Parent',axes_e);
-    image(left,'AlphaData',alpha_left,'Parent',axes_left);
-    image(down,'AlphaData',alpha_down,'Parent',axes_down);
-    image(up,'AlphaData',alpha_up,'Parent',axes_up);
-    image(right,'AlphaData',alpha_right,'Parent',axes_right);
-
-
+    display_key('w',btn_middle, 300, dialog_handle);
+    display_key('a',btn_left, 270, dialog_handle);
+    display_key('s',btn_middle, 270, dialog_handle);
+    display_key('d',btn_right, 270, dialog_handle);
+    display_key('q',btn_left, 200, dialog_handle);
+    display_key('e',btn_right, 200, dialog_handle);
+    display_key('left',btn_left, 100, dialog_handle);
+    display_key('up',btn_middle, 130, dialog_handle);
+    display_key('right',btn_right, 100, dialog_handle);
+    display_key('down',btn_middle, 100, dialog_handle);
 
    % Display info text
     instruction_wasd="Use W and S to rotate vertically, A and D to rotate horizontally";
@@ -79,29 +51,23 @@ function show_instructions(~, ~)
             'HorizontalAlignment', 'center',...
             'FontName', 'arial',...
             'FontWeight', 'bold',...
-            'FontSize', 9);
-       
-    % Hide all axes (must be done after image has been set)
-    axes_w.Visible='off';
-    axes_a.Visible='off';
-    axes_s.Visible='off';
-    axes_d.Visible='off';
-    axes_q.Visible='off';
-    axes_e.Visible='off';
-    axes_left.Visible='off';
-    axes_right.Visible='off';
-    axes_up.Visible='off';
-    axes_down.Visible='off';
-
-        
+            'FontSize', 9);    
     
-% Wait for d to close before running to completion
+    % Wait for d to close before running to completion
     uiwait(dialog_handle);
    
 end
 
-function [image, alpha]=get_key_image(button)
-    [image,~,alpha] = imread(strcat('instruction_images/',button,'.png')); 
+function display_key(char,x,y,dialog_handle)
+    side_len=30;
+    ax = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [x y side_len side_len]);
+    [im,alpha]=get_key_image(char);
+    image(im,'AlphaData',alpha,'Parent',ax);
+    ax.Visible='off';
+end
+
+function [image, alpha]=get_key_image(char)
+    [image,~,alpha] = imread(strcat('instruction_images/',char,'.png')); 
 end
 
 function display_info_text(text,handle, pos_y)
