3d7eff66157f34e1226176a42359712a535fb584
Shervin Koushan <shervink@stud.ntnu.no>
2022-07-15 21:56:12 +0200

    dialog almost done

--- show_instructions.m
+++ show_instructions.m
@@ -17,8 +17,12 @@ function show_instructions(~, ~)
             'FontWeight', 'bold',...
             'FontSize', 10);
        
-    % axes that span the whole dialog
-    hImageAxes = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [0 0 dialog_width dialog_height]);
+    % axes for each keyboard button
+    btn_len=30;
+    axes_w = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [75 330 btn_len btn_len]);
+    axes_a = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [40 300 btn_len btn_len]);
+    axes_s = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [75 300 btn_len btn_len]);
+    axes_d = axes('Parent', dialog_handle, 'Units', 'pixels', 'Position', [110 300 btn_len btn_len]);    
     
     % The buttons
     [w,alpha_w] = get_key_image('w');
@@ -27,36 +31,25 @@ function show_instructions(~, ~)
     [d,alpha_d] = get_key_image('d');
     [q,alpha_q] = get_key_image('q');
     [e,alpha_e] = get_key_image('e');
+    
+    
+    % Display the buttons
+    image(w,'AlphaData',alpha_w,'Parent',axes_w);
+    image(a,'AlphaData',alpha_a,'Parent',axes_a);
+    image(s,'AlphaData',alpha_s,'Parent',axes_s);
+    image(d,'AlphaData',alpha_d,'Parent',axes_d);
 
-   
+   % Display info text
     instruction_wasd="Use W and S to rotate vertically, A and D to rotate horizontally";
-
-    image(w,'AlphaData',alpha_w,'Parent',hImageAxes);
-    uicontrol('Parent',dialog_handle,'Style','text',...
-           'Position',[190 200 200 40],...
-           'String',instruction_wasd,...
-            'FontName', 'arial',...
-            'FontSize', 8);
-        
-    [x,~] = imread('instruction_images/q.png'); 
-    I2=imresize(x, [42 113]);
-    uicontrol('units','pixels','position',[40 300 113 42],'cdata',I2,'Parent',dialog_handle);
-    uicontrol('Parent',dialog_handle,'Style','text',...
-           'Position',[190 300 200 40],...
-           'String','Use Q and E to zoom in and out',...
-            'FontName', 'arial',...
-            'FontSize', 8);
-        
-    [x,~] = imread('instruction_images/e.png'); 
-    I2=imresize(x, [42 113]);
-    uicontrol('units','pixels','position',[40 100 113 42],'cdata',I2,'Parent',dialog_handle);
-    uicontrol('Parent',dialog_handle,'Style','text',...
-           'Position',[190 100 200 40],...
-           'String','Use the arrows to pan in the image',...
-            'FontName', 'arial',...
-            'FontSize', 8);
-          
-    btn = uicontrol('Parent',dialog_handle,...
+    instruction_qe="Use Q and E to zoom in and out";
+    instruction_arrows="Use the arrows to pan in the image";
+    
+    display_info_text(instruction_wasd,dialog_handle, 300);
+    display_info_text(instruction_qe,dialog_handle, 200);
+    display_info_text(instruction_arrows,dialog_handle, 100);
+       
+    % Close button
+    uicontrol('Parent',dialog_handle,...
            'Position',[dialog_width/2-35 20 70 35],...
            'String','Close',...
            'Callback','delete(gcf)',...
@@ -66,7 +59,11 @@ function show_instructions(~, ~)
             'FontWeight', 'bold',...
             'FontSize', 9);
        
-    hImageAxes.Visible='off';
+    % Hide all axes (must be done after image has been set)
+    axes_w.Visible='off';
+    axes_a.Visible='off';
+    axes_s.Visible='off';
+    axes_d.Visible='off';
     
 % Wait for d to close before running to completion
     uiwait(dialog_handle);
@@ -75,4 +72,12 @@ end
 
 function [image, alpha]=get_key_image(button)
     [image,~,alpha] = imread(strcat('instruction_images/',button,'.png')); 
+end
+
+function display_info_text(text,handle, pos_y)
+    uicontrol('Parent',handle,'Style','text',...
+           'Position',[190 pos_y 200 40],...
+           'String',text,...
+            'FontName', 'arial',...
+            'FontSize', 8);
 end
