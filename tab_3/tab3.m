b46ce8df19c905fd5fc39d9b643532296b2a73fb
Shervin Koushan <shervink@stud.ntnu.no>
2022-07-19 16:08:55 +0200

    reset  camera button

--- tab_3/tab3.m
+++ tab_3/tab3.m
@@ -38,9 +38,16 @@ function tab3(file_path, back_rec, top_rec, bottom_rec, left_rec, right_rec, dep
         'Style', 'pushbutton', 'HorizontalAlignment', 'center', ...
         'FontName', 'arial', 'FontWeight', 'bold', 'FontSize', 11);
 
-    %% Screenshot button
+    %% Reset button
     uicontrol('Parent', tab_handles{3, 1}, ...
     'Units', 'pixels', 'Position', [panel_width - 140 320 120 40], ...
+        'String', 'Reset', 'Callback', @reset, ...
+        'Style', 'pushbutton', 'HorizontalAlignment', 'center', ...
+        'FontName', 'arial', 'FontWeight', 'bold', 'FontSize', 11);
+
+    %% Screenshot button
+    uicontrol('Parent', tab_handles{3, 1}, ...
+    'Units', 'pixels', 'Position', [panel_width - 140 240 120 40], ...
         'String', 'Export image', 'Callback', {@take_screenshot, image_axes}, ...
         'Style', 'pushbutton', 'HorizontalAlignment', 'center', ...
         'FontName', 'arial', 'FontWeight', 'bold', 'FontSize', 11);
@@ -112,53 +119,12 @@ function keyPressCallback(~, eventdata)
 
     end
 
-    hide_planes_if_in_background
+    hide_planes_if_in_background();
 end
 
-function hide_planes_if_in_background
-    % This function ensures that we can still see inside the 3d dice,
-    % i.e. if we move around on one side that plane will be hidden so that
-    % we can see what is behind
-
-    global x_b;
-    global y_b;
-    global back_warp;
-    global top_warp;
-    global bot_warp;
-    global left_warp;
-    global right_warp;
-    cam_p = campos;
-
-    back_visible = 'on';
-    top_visible = 'on';
-    bot_visible = 'on';
-    left_visible = 'on';
-    right_visible = 'on';
-
-    if cam_p(3) < 0
-        back_visible = 'off';
-    end
-
-    if cam_p(2) < 0
-        top_visible = 'off';
-    end
-
-    if cam_p(2) > y_b
-        bot_visible = 'off';
-    end
-
-    if cam_p(1) < 0
-        left_visible = 'off';
-    end
-
-    if cam_p(1) > x_b
-        right_visible = 'off';
-    end
-
-    set(back_warp, 'visible', back_visible)
-    set(top_warp, 'visible', top_visible)
-    set(bot_warp, 'visible', bot_visible)
-    set(left_warp, 'visible', left_visible)
-    set(right_warp, 'visible', right_visible)
-
+function reset(~, ~)
+    % Set camera position and angle to initial values
+    reset_camera();
+    % This will restore any previously hidden planes
+    hide_planes_if_in_background();
 end
